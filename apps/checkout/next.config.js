9dd3e7c5d6d12aa952eaaac1bfd1597b0527e4e4
HamzaBakkour <alexander.x4.hb@outlook.com>
2024-02-02 07:10:41 +0100

    init commit

---
+++ apps/checkout/next.config.js
@@ -0,0 +1,39 @@
+const NextFederationPlugin = require("@module-federation/nextjs-mf");
+// this enables you to use import() and the webpack parser
+// loading remotes on demand, not ideal for SSR
+const remotes = (isServer) => {
+  const location = isServer ? "ssr" : "chunks";
+  return {
+    catalog: `checkout@http://localhost:3002/_next/static/${location}/remoteEntry.js`,
+  };
+};
+
+const path = require("path");
+
+module.exports = {
+
+  output: "standalone",
+  
+  experimental : {
+    outputFileTracingRoot: path.join(__dirname, "../../"),
+  },
+
+  webpack(config, options) {
+    config.plugins.push(
+      new NextFederationPlugin({
+        name: "checkout",
+        filename: "static/chunks/remoteEntry.js",
+        exposes: {
+          "./Module": "./pages/index.tsx",
+        },
+        remotes: remotes(options.isServer),
+        shared: {},
+        extraOptions: {
+          automaticAsyncBoundary: true,
+        },
+      })
+    );
+
+    return config;
+  },
+};
