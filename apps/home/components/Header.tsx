421e7bc105e86a2a08f64e5ae6b46e3e4fd5f09f
HamzaBakkour <alexander.x4.hb@outlook.com>
2024-02-02 09:15:23 +0100

    mc9

--- apps/home/components/Header.tsx
+++ apps/home/components/Header.tsx
@@ -5,7 +5,7 @@ export function Header() {
     <Menu fixed="top" inverted>
       <Container>
         <Menu.Item as="a" header>
-          eBuy.comxx
+          eBuy.comxxd
         </Menu.Item>
         <MenuItems />
         <Menu.Item position="right">
